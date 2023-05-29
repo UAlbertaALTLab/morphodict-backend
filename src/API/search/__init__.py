7b20a2656f584e1144360a25c59a8a45ef2ec3c1
M1Al3x <kasych@ualberta,ca>
2023-05-28 22:35:47 -0600

    imporvement in speed and phrase search

--- src/API/search/__init__.py
+++ src/API/search/__init__.py
@@ -6,8 +6,8 @@ def search_with_affixes(
     rw_index: str,
     rw_domain: str,
     wn_synset: str,
-    include_auto_definitions=False,
-    inflect_english_phrases=False,
+    include_auto_definitions=True,
+    inflect_english_phrases=True,
 ):
     """
     Search for wordforms matching:
